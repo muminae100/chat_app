d19b36ba2f2af053b9957f5ba0ae255c337f15b4
foreverkenyan <smuminaetx100@gmail.com>
2022-04-03 12:49:25 +0300

    Implement logout

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,11 +1,32 @@
 import React from 'react';
-
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../components/firebase';
+ 
 function Chats() {
+  const history = useHistory();
+  const handleLogout = async () =>{
+    await auth.signOut();
+    history.push('/');
+  }
   return (
-    <h1>
-        Chats
-    </h1>
-  )
+    <div className="chats-page">
+        <div className="nav-bar">
+            <div className="logo-tab">
+                Chat app
+            </div>
+            <div onClick={handleLogout} className="logout-tab">
+                Logout
+            </div>
+        </div>
+        <ChatEngine 
+        height = "calc(100vh - 66px)"
+        projectId = "cbb27171-c896-42ed-918c-96d3a4e101e6"
+        userName = "."
+        userSecret = "."    
+        />
+    </div>
+  );
 }
 
 export default Chats;
