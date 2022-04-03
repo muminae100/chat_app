de17e021885bffaee6cc9eff65ce8575cf1333a8
foreverkenyan <smuminaetx100@gmail.com>
2022-04-03 19:35:06 +0300

    Deploy to netlify

---
+++ build/precache-manifest.ee8eadf9afe8e4377e381c7a1266716e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de33c8fc0da21243f2db8cb3deffef8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c519a4ed705b12b3655",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "8e8d98a003e6f6789bb6",
+    "url": "/static/js/2.91de3f3c.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.91de3f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c519a4ed705b12b3655",
+    "url": "/static/js/main.df26a2da.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
